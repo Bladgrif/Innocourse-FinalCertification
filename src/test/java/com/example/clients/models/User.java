f145756fa00b6e58557175bb736f37b120770405
Roman Grigorev <roma@gmail.com>
2024-06-26 14:31:30 +0300

    get token

--- src/test/java/com/example/clients/models/User.java
+++ src/test/java/com/example/clients/models/User.java
@@ -1,27 +1,42 @@
 package com.example.clients.models;
 
-public class User {
-    private String name;
-    private String password;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-    public User(String name, String password) {
-        this.name = name;
-        this.password = password;
-    }
+public class User{
 
-    public String getName() {
-        return name;
-    }
+	@JsonProperty("password")
+	private String password;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	@JsonProperty("username")
+	private String username;
 
-    public String getPassword() {
-        return password;
-    }
+	public User(String username,String password) {
+		this.password = password;
+		this.username = username;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setPassword(String password){
+		this.password = password;
+	}
+
+	public String getPassword(){
+		return password;
+	}
+
+	public void setUsername(String username){
+		this.username = username;
+	}
+
+	public String getUsername(){
+		return username;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"User{" + 
+			"password = '" + password + '\'' + 
+			",username = '" + username + '\'' + 
+			"}";
+		}
 }
